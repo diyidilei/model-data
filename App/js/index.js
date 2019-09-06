1146a78af892c82a376a681c6e5671c92844137a
diyidilei <35208524+diyidilei@users.noreply.github.com>
2019-09-06 11:29:47 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -79,6 +79,7 @@ xui.Class('App', 'xui.Module',{
             append(
                 xui.create("xui.UI.ComboInput")
                 .setHost(host,"xui_ui_comboinput285")
+                .setName("num")
                 .setDirtyMark(false)
                 .setLeft("12.5em")
                 .setTop("14.166666666666666em")
