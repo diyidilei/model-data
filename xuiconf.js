9380bb67c267dcc5630a6f66005b9a76b32dcda6
diyidilei <35208524+diyidilei@users.noreply.github.com>
2019-09-06 14:12:34 +0800

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -54,6 +54,9 @@ xui.$cache.functions = {
         "actions":[
             function(num1, num2){
                 return(+num1)*(+num2)
+            },
+            function(num1, num2){
+                return(+num1)*(+num2)
             }
         ]
     }
